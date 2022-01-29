d3149a93ce4d42386a117458180b91e0dee05f95
2565259386@qq.com <xia20051220>
2022-01-30 07:56:55 +0800

    Update FlyCommand.java

---
+++ src/zishu/xiatools/Commands/FlyCommand.java
@@ -0,0 +1,35 @@
+package zishu.xiatools.Commands;
+
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import zishu.xiatools.XIATools;
+
+public class FlyCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return true;
+        }
+        Player p = (Player) sender;
+        if (!p.hasPermission("XIATools.Fly")) {
+            p.sendMessage(XIATools.prefix + XIATools.noPermission);
+            return true;
+        }
+        if (p.getAllowFlight()) {
+            p.setAllowFlight(false);
+            p.setFlying(false);
+            p.sendMessage("XIATools.prefix + 飞行模式已禁用");
+            p.playSound(p.getLocation(), Sound.ENTITY_BAT_DEATH, 3.0F, 2.0F);
+        } else {
+            p.setAllowFlight(true);
+            p.setFlying(true);
+            p.sendMessage("XIATools.prefix + 飞行模式已启用");
+            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 3.0F,2.0F);
+        }
+        return true;
+    }
+}
+
--- src/zishu/xiatools/XIATools.java
+++ src/zishu/xiatools/XIATools.java
@@ -1,10 +1,17 @@
 package zishu.xiatools;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class XIATools extends JavaPlugin {
+
+    public static String prefix = "§7[§bXIA&3Tools§7]";
+    public static String noPermission = "§你没有使用该命令的权限";
+
+
     @Override
     public void onEnable(){
         getLogger().info("已加载");
+        FileConfiguration config = getConfig();
     }
 }
