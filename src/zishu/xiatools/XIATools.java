6c85a8e48d5324595417c08c9451a797d445f7e4
2565259386@qq.com <xia20051220>
2022-01-30 06:56:29 +0800

    XIATools

---
+++ src/zishu/xiatools/XIATools.java
@@ -0,0 +1,10 @@
+package zishu.xiatools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class XIATools extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        getLogger().info("已加载");
+    }
+}
